b5400c19bf8c2c67a79a66f4f83ea40c0d7ffe24
ishita169 <ishuarials@gmail.com>
2018-12-23 09:29:29 +0530

    added new files in technical blog

---
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -0,0 +1,28 @@
+package technicalblog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import technicalblog.model.Post;
+import technicalblog.service.PostService;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private PostService postService;
+
+    @RequestMapping("/")
+    public String getAllPost(Model model) {
+
+        ArrayList<Post> posts =postService.getAllPosts();
+
+         model.addAttribute("posts",posts);
+        return "index";
+
+    }
+}
